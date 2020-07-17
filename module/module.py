23328789eb9c7b49ceb6d91cff20f60b4177d238
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2020-07-18 01:05:50 +0700

    Add files via upload

---
+++ module/module.py
@@ -0,0 +1,19 @@
+from datetime import timedelta, datetime, date
+from bs4 import BeautifulSoup
+from googletrans import Translator
+from kbbi import KBBI
+from random import randint
+from liff.ttypes import LiffChatContext, LiffContext, LiffSquareChatContext, LiffNoneContext, LiffViewRequest
+import pytz, pafy, time, asyncio, random, multiprocessing, timeit, sys, json, ctypes, codecs, threading, glob, re, ast, six, os, subprocess, wikipedia, atexit, urllib, urllib.parse, urllib3, string, tempfile, shutil, unicodedata
+from humanfriendly import format_timespan, format_size, format_number, format_length
+import html5lib
+import requests,json,urllib3
+from random import randint
+from bs4 import BeautifulSoup
+from gtts import gTTS
+from googletrans import Translator
+import youtube_dl
+from time import sleep
+import pyimgflip
+from zalgo_text import zalgo
+from threading import Thread,Event
