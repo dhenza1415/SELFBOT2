dc3d391b75fdd79ee08c545728d28cb6e81a1b5b
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2020-07-21 15:33:27 +0700

    Add files via upload

---
+++ module/module.py
@@ -0,0 +1,23 @@
+from DATA.ttypes import Message
+from DATA.ttypes import ContentType as Type
+from DATA.ttypes import ChatRoomAnnouncementContents
+from DATA.ttypes import ChatRoomAnnouncement
+from thrift import transport, protocol, server
+from thrift.unverting import *
+from thrift.TMultiplexedProcessor import *
+from thrift.TSerialization import *
+from thrift.TRecursive import *
+from datetime import datetime, timedelta
+import pytz, pafy, time, asyncio, random, multiprocessing, timeit, sys, json, ctypes, codecs, threading, glob, re, ast, six, os, subprocess, wikipedia, atexit, urllib, urllib.parse, urllib3, string, tempfile, shutil, unicodedata
+from humanfriendly import format_timespan, format_size, format_number, format_length
+import html5lib
+import requests,json,urllib3
+from random import randint
+from bs4 import BeautifulSoup
+from gtts import gTTS
+from googletrans import Translator
+import youtube_dl
+from time import sleep
+import pyimgflip
+from zalgo_text import zalgo
+from threading import Thread,Event
