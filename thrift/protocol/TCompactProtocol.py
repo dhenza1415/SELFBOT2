53396bc25d4c28ea3b497299b1bc20c4dde5e744
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2020-07-25 19:16:21 +0700

    Update TCompactProtocol.py

--- thrift/protocol/TCompactProtocol.py
+++ thrift/protocol/TCompactProtocol.py
@@ -1,6 +1,3 @@
-#LICENCE :   http://www.apache.org/licenses/LICENSE-2.0
-#CREATOR BY : PRANKBOT
-#MOD BY ACIL
 from .TProtocol import TType, TProtocolBase, TProtocolException, checkIntegerLimits
 from struct import pack, unpack
 from ..compat import binary_to_str, str_to_binary
@@ -368,6 +365,3 @@ class TCompactProtocolAcceleratedFactory(object):
             string_length_limit=self.string_length_limit,
             container_length_limit=self.container_length_limit,
             fallback=self._fallback)
-#LICENCE :   http://www.apache.org/licenses/LICENSE-2.0
-#CREATOR BY : PRANKBOT
-#MOD BY ACIL
