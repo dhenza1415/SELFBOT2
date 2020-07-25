41cd4c59cd08764614166e3a5886659dbc9b53c8
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2020-07-25 19:19:39 +0700

    Update __init__.py

--- thrift/protocol/__init__.py
+++ thrift/protocol/__init__.py
@@ -1,4 +1,1 @@
-#LICENCE :   http://www.apache.org/licenses/LICENSE-2.0
-#CREATOR BY : PRANKBOT
-#MOD BY ACIL
 __all__ = ['fastbinary', 'TBase', 'TBinaryProtocol', 'TCompactProtocol', 'TJSONProtocol', 'TProtocol']
