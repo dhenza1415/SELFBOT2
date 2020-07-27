76a86a0933a7c141d3fe094d49f5cdaf39a608c9
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2020-07-27 14:26:36 +0700

    Add files via upload

--- module/module.py
+++ module/module.py
@@ -1,23 +1,28 @@
-from DATA.ttypes import Message
-from DATA.ttypes import ContentType as Type
-from DATA.ttypes import ChatRoomAnnouncementContents
-from DATA.ttypes import ChatRoomAnnouncement
-from thrift import transport, protocol, server
-from thrift.unverting import *
-from thrift.TMultiplexedProcessor import *
-from thrift.TSerialization import *
-from thrift.TRecursive import *
-from datetime import datetime, timedelta
-import pytz, pafy, time, asyncio, random, multiprocessing, timeit, sys, json, ctypes, codecs, threading, glob, re, ast, six, os, subprocess, wikipedia, atexit, urllib, urllib.parse, urllib3, string, tempfile, shutil, unicodedata
-from humanfriendly import format_timespan, format_size, format_number, format_length
-import html5lib
-import requests,json,urllib3
-from random import randint
+from datetime import timedelta, datetime, date
 from bs4 import BeautifulSoup
-from gtts import gTTS
 from googletrans import Translator
+from kbbi import KBBI
+from random import randint
+import pytz 
+import time
+import timeit
+import random
+import sys
+import ast
+import re
+import os
+import json
+import livejson
+import subprocess
+import threading
+import string
+import codecs
+import requests
+import ctypes
+import urllib
+import wikipedia
+import html5lib
+import http.client
+import mimetypes
+import humanize
 import youtube_dl
-from time import sleep
-import pyimgflip
-from zalgo_text import zalgo
-from threading import Thread,Event
